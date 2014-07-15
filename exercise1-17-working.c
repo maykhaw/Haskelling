bb6f4ef9e905a53f38c8ac85da1c9205ef156452
Khaw Swee May <SweeMayKhaw@gmail.com>
2014-07-15 20:52:17 +0800

    works

--- exercise1-17-working.c
+++ exercise1-17-working.c
@@ -2,25 +2,24 @@
 
 /* print all input lines longer than longline=80 characters */ 
 
-#define	longline	80	/* longline is the number at which all lines are to be printed out */ 
+#define	longline	20	/* longline is the number at which all lines are to be printed out */ 
 
 
-int main()
-{
+int main() {
 	int i = 0;
 	int c = '\n';
 	char s[longline]; 
 	
-	while (c!=EOF){
-
+	while (c!=EOF) {
 		for (i=0; (c=getchar())!=EOF && c!='\n' && i < longline; ++i)
 			s[i]=c; 
 		
 		if (i >= longline){
 			for (i = 0; i < longline; ++i)
 				printf("%c", s[i]);
-			while ((c=getchar())!=EOF && c!='\n')
+			do {
 				printf("%c", c);
+			} while ((c != '\n') && (c=getchar())!=EOF ); 
 		}
 	}
-}	
+}
