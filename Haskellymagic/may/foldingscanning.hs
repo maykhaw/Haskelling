105f9af8c108569b59fd8b6963206038b384e3e2
May Khaw <sweemaykhaw@gmail.com>
2014-11-09 15:07:07 +1100

    nearly at scanr

--- Haskellymagic/may/foldingscanning.hs
+++ Haskellymagic/may/foldingscanning.hs
@@ -1,5 +1,6 @@
 import Test.QuickCheck
 import Test.QuickCheck.Function
+import Data.List
 
 scanl' :: (a -> b -> a) -> a -> [b] -> [a]
 scanl' f initial [] = [initial]
@@ -10,8 +11,45 @@ testscanl f' init l = scanl' f init l == scanl f init l where
     f a b = apply f' (a,b)
 
 scanr' :: (a -> b -> b) -> b -> [a] -> [b] 
-scanr' f initial [] = [initial]
-scanr' f initial (x : xs) = _ : scanr' f initial xs
+scanr' f terminal [] = [terminal]
+scanr' f terminal [a] = f a terminal : terminal
+scanr' f terminal [a, b] = let newb = f b terminal in 
+                           f a newb : newb : terminal 
+scanr' f terminal [c,b,a] = let newa = f a terminal
+                                newb = f b newa
+                                newc = f c newb in
+                            newc : newb : newa : terminal 
+scanr' f terminal [d,c,b,a] = let newa = f a terminal
+                                  newb = f b newa
+                                  newc = f c newb 
+                                  newd = f d newc in
+                              newd : newc : newb : newa : terminal 
+scanr' f terminal list = let sr l [] = l 
+                             sr l (y : ys) = sr ys (y : l) in
+                          sr 
+                           
 
-main = 
+testscanr :: Fun (Int, Int) Int -> Int -> [Int] Bool
+tescscanr f' term l = scanr' f term l == scanr f term l where
+    f a b = apply f' (a,b) 
+
+manytail :: [a] -> [[a]] 
+manytail [] = [[]]
+manytail (x : xs) = (x : xs) : manytail xs 
+
+testtails :: [Char] -> Bool
+testtails l = manytail l == tails l  
+
+
+
+rev :: [a] -> [a] 
+rev [] = [] 
+rev [a] = [a] 
+rev (x : xs) = rev xs ++ [x] 
+
+testreverse :: [Char] -> Bool
+testreverse l = rev l == reverse l 
+main = do
     quickCheck testscanl 
+    quickCheck testreverse
+    quickCheck testtails 
