58fd89746b7ab72e0a8a4edf8898024d50ec685f
May Khaw <sweemaykhaw@gmail.com>
2014-11-11 17:11:47 +1100

    cakethief now works, with bruteforce

--- Interview Cake/cakethief.hs
+++ Interview Cake/cakethief.hs
@@ -5,7 +5,8 @@ import Control.Arrow
 
 --maxCake takes a max weight and a tuple (weight of cake, value of cake) and returns (current max total, remaining weight of cake) 
 maxCake ::  Int -> ( Int,  Int) -> ( Int, Int) 
-maxCake _ (0,_) = (0,0)
+maxCake maxweight (0,_) = (0,maxweight)
+maxCake maxweight (_,0) = (0,maxweight)
 maxCake 0 _ = (0,0)
 maxCake maxweight (kg,value) = let (cakeweight, remainder) = maxweight `divMod` kg
                                in (cakeweight * value, remainder) 
@@ -19,9 +20,9 @@ stolenlist maxweight (x : xs) = let (cakevalue, newmaxweight) = maxCake maxweigh
 maxduffle :: Int -> [(Int, Int)] -> Maybe Int
 maxduffle _ [] = Nothing
 maxduffle 0 _ = Nothing
-maxduffle weight l = let -- newl = zip l (map (\(x,y) -> y % x) l) 
-                         cakelist = reverse $ sortBy cmp l
-                         cmp (a,b) (c,d) = compare (a*d) (c * b)
+maxduffle weight l = let newl = filter (\(_,y) -> y /= 0) l
+                         cakelist = reverse $ sortBy cmp newl
+                         cmp (a,b) (c,d) = compare (b * c) (d * a) 
                      in Just $ sum $ stolenlist weight cakelist
 
 testadd :: NonNegative Int -> [(NonNegative Int, NonNegative Int)] -> (NonNegative Int, NonNegative Int) -> Bool  
@@ -29,6 +30,24 @@ testadd (NonNegative x) abs ab = maxduffle x (fmap unwrap' abs) <= maxduffle x (
     where unwrap (NonNegative i) = i
           unwrap' = unwrap *** unwrap
 
+-- bruteforce: how to write a function that takes a maximum x and a list of Ints and generates all possible combinations that add up to x? 
+
+maxgen :: Int -> [(Int,Int)] -> [[(Int,Int)]]
+maxgen duffle _ | duffle <= 0 = []
+maxgen _ [] = [] 
+maxgen duffle (cake : cakelist) = (map (cake :) $ maxgen (duffle - (fst cake)) (cake : cakelist)) ++ maxgen duffle cakelist 
+
+maxgenduffle :: Int -> [(Int,Int)] -> Maybe Int
+maxgenduffle 0 _ = Nothing
+maxgenduffle _ [] = Nothing 
+maxgenduffle duffle list = let values (x : xs) = map snd x : values xs 
+                           in Just $ maximum $ map sum $ values $ maxgen duffle list 
+
+testmax :: NonNegative Int -> [(NonNegative Int, NonNegative Int)] -> Bool  
+testmax (NonNegative max) list = maxgenduffle max (fmap unwrap' list) == maxduffle max (fmap unwrap' list)
+    where unwrap (NonNegative i) = i
+          unwrap' = unwrap *** unwrap
 main :: IO ()
 main = do
     quickCheck testadd
+    quickCheck testmax
