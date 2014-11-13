b16f9c8dfde13ef9ed7c485951e2aa53a1d314d0
May Khaw <sweemaykhaw@gmail.com>
2014-11-13 19:10:13 +1100

    cakethiefing

--- Interview Cake/cakethief.hs
+++ Interview Cake/cakethief.hs
@@ -2,6 +2,7 @@ import Data.Ord
 import Data.List
 import Test.QuickCheck
 import Control.Arrow
+import qualified Data.Set as Set
 
 --maxCake takes a max weight and a tuple (weight of cake, value of cake) and returns (current max total, remaining weight of cake) 
 maxCake ::  Int -> ( Int,  Int) -> ( Int, Int) 
@@ -25,29 +26,75 @@ maxduffle weight l = let newl = filter (\(_,y) -> y /= 0) l
                          cmp (a,b) (c,d) = compare (b * c) (d * a) 
                      in Just $ sum $ stolenlist weight cakelist
 
-testadd :: NonNegative Int -> [(NonNegative Int, NonNegative Int)] -> (NonNegative Int, NonNegative Int) -> Bool  
-testadd (NonNegative x) abs ab = maxduffle x (fmap unwrap' abs) <= maxduffle x (fmap unwrap' $ ab : abs) 
-    where unwrap (NonNegative i) = i
-          unwrap' = unwrap *** unwrap
+testadd :: Positive Int -> CakeList -> (Positive Int, Positive Int) -> Bool  
+testadd (Positive x) (CakeList abs) ab = maxduffle x abs <= maxduffle x (unwrap' ab : abs) 
+
+testadd' :: Positive Int -> CakeList -> (Positive Int, Positive Int) -> Bool  
+testadd' (Positive x) (CakeList abs) ab = maxgenduffle x abs <= maxgenduffle x (unwrap' ab : abs) 
+
 
 -- bruteforce: how to write a function that takes a maximum x and a list of Ints and generates all possible combinations that add up to x? 
 
-maxgen :: Int -> [(Int,Int)] -> [[(Int,Int)]]
-maxgen duffle _ | duffle <= 0 = []
-maxgen _ [] = [] 
-maxgen duffle (cake : cakelist) = (map (cake :) $ maxgen (duffle - (fst cake)) (cake : cakelist)) ++ maxgen duffle cakelist 
+value (_,v) = v
+weight (w,_) = w
+
+testMaxgen :: Positive Int -> CakeList -> Bool
+testMaxgen (Positive i) (CakeList l) = all (<=i) . map totalWeight $ maxgen i l where
+    totalWeight = sum . map weight
+
+-- This easily uses oodles of memory.
+-- Brutest force.
+maxgen', maxgen :: Int -> [(Int,Int)] -> [[(Int,Int)]]
+maxgen' duffle _ | duffle < 0 = []
+maxgen' 0 _ = [[]]
+maxgen' _ [] = [[]] 
+maxgen' duffle (cake : cakelist) = map (cake :) (maxgen' (duffle - weight cake) (cake : cakelist)) ++ maxgen' duffle cakelist 
+
+maxgen d l = maxgen' d (clean l)
+
+dominated a b = weight a >= weight b && value a <= value b
+
+type Cake = (Int, Int)
+
+rmDominatedCakes :: [Cake] -> [Cake]
+rmDominatedCakes = map fst . filter (\(c, cs) -> all (not . dominated c) cs) . allOthers
+
+rmDupes :: [Cake] -> [Cake]
+rmDupes = Set.toList . Set.fromList
+
+clean = rmDominatedCakes . rmDupes
+
+-- TODO: test.
+allOthers :: [a] -> [(a,[a])]
+allOthers list = zipWith3 (\l x r -> (x, l ++ r)) (inits list) list (tail $ tails list)
+
+testAllOthers :: [Int] -> Bool
+testAllOthers l = all (\(x,xs) -> sort (x:xs) == sort l) $ allOthers l
 
 maxgenduffle :: Int -> [(Int,Int)] -> Maybe Int
 maxgenduffle 0 _ = Nothing
 maxgenduffle _ [] = Nothing 
-maxgenduffle duffle list = let values (x : xs) = map snd x : values xs 
+maxgenduffle duffle list = let values = (map . map) value
                            in Just $ maximum $ map sum $ values $ maxgen duffle list 
 
-testmax :: NonNegative Int -> [(NonNegative Int, NonNegative Int)] -> Bool  
-testmax (NonNegative max) list = maxgenduffle max (fmap unwrap' list) == maxduffle max (fmap unwrap' list)
-    where unwrap (NonNegative i) = i
-          unwrap' = unwrap *** unwrap
+-- testmax :: Positive Int -> [(Positive Int, Positive Int)] -> Bool  
+testmax :: Positive Int -> CakeList -> Bool  
+testmax (Positive max) (CakeList list) = maxgenduffle max list >= maxduffle max list
+
+unwrap (Positive i) = i
+unwrap' = (unwrap *** unwrap)
+
+data CakeList = CakeList [(Int,Int)] deriving (Show, Eq, Ord)
+
+instance Arbitrary CakeList where
+    arbitrary = fmap (CakeList . fmap unwrap') arbitrary
+
+
+
 main :: IO ()
 main = do
+    quickCheck testAllOthers 
     quickCheck testadd
+    quickCheck testadd'
     quickCheck testmax
+    quickCheck testMaxgen
