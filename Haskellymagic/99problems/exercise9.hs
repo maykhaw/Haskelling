87ef98176ece53fc1b986692e66892f2f62e2795
Khaw Swee May <SweeMayKhaw@gmail.com>
2014-11-18 19:03:50 +0800

    re-implement group / packing

--- Haskellymagic/99problems/exercise9.hs
+++ Haskellymagic/99problems/exercise9.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE TemplateHaskell #-}
 import Data.List
 import Test.QuickCheck 
 
@@ -6,33 +7,47 @@ packing [] = []
 packing (x : xs) = let (before,after) = break (/= x) (x : xs) in 
                    before : packing after 
 
-recursepack :: Eq a => [a] -> [[a]] 
-recursepack [] = [] 
-recursepack (x : xs) = case xs of
-    [] -> [[x]]
-    (y : ys) -> if y == x then x : y : case ys of
-                                [] -> [[x,y]]
-                                [a] -> if a == y then [[x,y,b]]
-                                                 else [x,y] 
-                                                     : recursepackys
-                                (b : bs) -> if b == y then x
-                          else [x] : recursepack (y : ys) 
+-- recursepack :: Eq a => [a] -> [[a]] 
+-- recursepack [] = [] 
+-- recursepack (x : xs) = case xs of
+--     [] -> [[x]]
+--     (y : ys) -> if y == x then x : y : case ys of
+--                                 [] -> [[x,y]]
+--                                 [a] -> if a == y then [[x,y,b]]
+--                                                  else [x,y] 
+--                                                      : recursepackys
+--                                 (b : bs) -> if b == y then x
+--                           else [x] : recursepack (y : ys) 
 
-testpack1 :: [Char] -> Bool
-testpack1 l = l == concat (packing l) 
-testpack2 :: [Char] -> Bool
-testpack2 l = case packing l of
+rpack :: Eq a => [a] -> [[a]]
+rpack [] = []
+rpack (x : xs) = case rpack xs of
+    [] -> [[x]]
+    (y : ys) -> case y of
+        [] -> error "Can never happen" -- [x] : ys
+        (z : zs) -> if x == z
+                    then   (x : z : zs) : ys
+                    else [x] : (z : zs) : ys
+
+prop_rpack :: [Int] -> Property
+prop_rpack l = rpack l === packing l
+
+prop_pack1 :: [Char] -> Bool
+prop_pack1 l = l == concat (packing l) 
+
+prop_pack2 :: [Char] -> Bool
+prop_pack2 l = case packing l of
     (x : y : xs) -> last x /= head y
     _ -> True
 
-testpack3 :: [Char] -> Bool
-testpack3 l = group l == packing l
+prop_pack3 :: [Char] -> Bool
+prop_pack3 l = group l == packing l
 
-testpack4 :: [Char] -> Bool 
-testpack4 l = group l == recursepack l
+-- prop_pack4 :: [Char] -> Bool 
+-- prop_pack4 l = group l == recursepack l
+
+return []
+runTests = $quickCheckAll
 
 main = do
-    quickCheck testpack1
-    quickCheck testpack2
-    quickCheck testpack3
-    quickCheck testpack4
+    runTests
