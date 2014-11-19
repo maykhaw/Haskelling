d7400630330b9ba6cb54bf36564f8140712b7be8
May Khaw <sweemaykhaw@gmail.com>
2014-11-19 23:22:41 +1100

    new errors in parenthese

--- Haskellymagic/Matthias/parenthese.hs
+++ Haskellymagic/Matthias/parenthese.hs
@@ -1,35 +1,38 @@
-{-# LANGUAGE TemplateHaskell #-} 
+{-# LANGUGE TemplateHaskell #-} 
 import Test.QuickCheck 
 import Data.List 
 
--- parents :: [Char] -> Bool 
--- parents [] = True
--- parents [a] = False 
--- parents (x : xs) = let helper [] a = [a]
---                        helper l a = case a of
---                                     '(' -> l ++ [a] 
---                                     ')' -> if last l = '(' then init l
---                                                            else False in
---                    helper [] x : parents xs  
---                                      
-
-parents :: [Char] -> [Char] 
+parents :: [Char] -> Int 
 parents l = foldl (\a b -> case b of
-                           '(' -> a ++ [b]
-                           ')' -> init a
-                           _ -> a) [] l 
+                           '(' -> a + 1
+                           ')' -> a - 1
+                           _ -> a) 0 l
 
-prop_length :: [Bool] -> Bool 
-prop_length l = let newlist = map (\t -> if t then '(' else ')') l
-                    (open, close) = partition (== '(') newlist 
-                    (op, cl) = (length open, length close) in
-                case null (parents newlist) of
-                    True -> op == cl
-                    False -> op /= cl 
-               
-               
---prop_2 :: [Char] -> [Char] -> Bool 
+parentheses :: [Char] -> Bool
+parentheses [] = True
+parentheses [_] = False
+parentheses (x : xs)  = if x == ')' then False else parents (x : xs) == 0 
 
+fromBool :: [Bool] -> [Char]
+fromBool l = map (\t -> if t then '(' else ')') l 
+
+prop_length :: [Bool] -> Bool
+prop_length l = let newlist = fromBool l 
+                    (open,close) = partition (== '(') newlist
+                    (op,cl) = (length open, length close) in 
+                if parentheses newlist then op == cl else op /= cl 
+                    
+prop_2 :: [Bool] -> [Bool] -> Bool 
+prop_2 xs ys = let (x, y) = (fromBool xs, fromBool ys) in
+               case (parentheses x, parentheses y) of
+                    (True,True) -> parentheses (x ++ y) == True
+                    (True,False) -> parentheses (x ++ y) == False 
+                    (False,True) -> parentheses (x ++ y) == False 
+                    (False,False) -> True
+                   
+prop_rev :: [Bool] -> Bool 
+prop_rev l = let newlist = reverse $ fromBool $ map not l in
+             parentheses newlist == parentheses (fromBool l)  
 return [] 
 runTests = $quickCheckAll 
 
