929258d0c3c6a96dc18e6b9bfe469d791daceb7d
May Khaw <sweemaykhaw@gmail.com>
2014-11-20 23:10:57 +1100

    new exercise 13

---
+++ Haskellymagic/99problems/exercise13.hs
@@ -0,0 +1,21 @@
+{-# LANGUAGE TemplateHaskell #-}
+import Test.QuickCheck
+
+encode :: Eq a => [a] -> [Either a (Int, a)]
+encode [] = []
+{-encode (x : xs) = case encode xs of
+    [] -> [Left x]
+    [a] -> if x == a then [Right (2,x)]
+                     else [Left x, Left a]
+    (y : ys) -> if x == y then case ys of
+                               [] -> [Right (2,x)]
+                               (b : bs) -> -}
+packing :: Eq a => [a] -> [[a]]
+packing [] = [] 
+packing (x : y : xs) = if x == y then x : y : packing xs 
+                                 else [x] : packing (y : xs)                              
+
+return [] 
+runTests = $quickCheckAll
+
+main = runTests 
