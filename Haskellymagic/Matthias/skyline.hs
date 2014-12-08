e9c37751043638b2bed4aba29eb34710920f0c63
May Khaw <sweemaykhaw@gmail.com>
2014-12-08 16:57:54 +0000

    updated skyline

--- Haskellymagic/Matthias/skyline.hs
+++ Haskellymagic/Matthias/skyline.hs
@@ -9,7 +9,7 @@ data Coords = Coords Int Int deriving (Ord, Eq, Show)
 instance Arbitrary Rectangle where
     arbitrary = do
         NonNegative h <- arbitrary
-        left <- arbitrary
+        NonNegative left <- arbitrary
         NonNegative width <- arbitrary
         return $ Rectangle left (left + width) h
 
@@ -19,6 +19,10 @@ instance Arbitrary Coords where
         NonNegative y <- arbitrary
         return $ Coords x y
    
+doubleHeight :: Rectangle -> Rectangle 
+doubleHeight (Rectangle a b c) = Rectangle a b (2 * c) 
+
+
 rectCoord :: Rectangle -> [Coords] 
 rectCoord (Rectangle x y z) = [Coords x 0, Coords x z, Coords y 0, Coords y z] 
 
@@ -31,13 +35,14 @@ coordList l = concatMap rectCoord l
 
 overlap :: Rectangle -> Rectangle -> Maybe Rectangle 
 overlap (Rectangle a b c) (Rectangle x y z) = let height = min c z in 
-                                              case sort [a,b,x,y] of 
-                                                   [_,x,b,_] -> if x == b then Nothing else Just $ Rectangle x b height 
-                                                   [_,x,y,_] -> Just $ Rectangle x y height 
-                                                   [_,a,y,_] -> if a == y then Nothing else Just $ Rectangle a y height 
-                                                   [_,a,b,_] -> Just $ Rectangle a b height 
-                                                   [_,_,_,_] -> Nothing 
-
+                                              if Rectangle a b c == Rectangle x y z then Just $ Rectangle x y z else 
+                                                case sort [a,b,x,y] of 
+                                                    [a,b,x,y] -> Nothing 
+                                                    [a,x,b,y] -> if x == b then Nothing else Just $ Rectangle x b height
+                                                    [a,x,y,b] -> Just $ Rectangle x y height 
+                                                    [x,y,a,b] -> Nothing 
+                                                    [x,a,y,b] -> if a == y then Nothing else Just $ Rectangle a y height 
+                                                    [x,a,b,y] -> Just $ Rectangle a b height 
 prop_overlap1 :: Rectangle -> Rectangle -> Bool 
 prop_overlap1 a b = overlap a b == overlap b a 
 
@@ -46,14 +51,33 @@ combomaker [] = []
 combomaker [_] = []
 combomaker (x : xs) = map (\y -> (x,y)) xs ++ combomaker xs 
 
+removeDups :: Eq a => [a] -> [a] 
+removeDups [] = [] 
+removeDups [a] = [a] 
+removeDups (x : xs) = x : removeDups (filter (/=x) xs) 
+
 overlapList :: [Rectangle] -> [Rectangle] 
-overlapList l = map fromJust $ filter (/= Nothing) $ map (uncurry overlap) $ combomaker l  
+overlapList l = mapMaybe (uncurry overlap) $ combomaker $ removeDups l  
 
 area :: Rectangle -> Int 
+
 area (Rectangle x y z) = z * (y - x) 
 
 skylinearea :: [Rectangle] -> Int 
-skylinearea l = sum (map area l) - sum (map area (overlapList l))
+skylinearea l = let newlist = removeDups l in 
+                sum (map area newlist) - sum (map area (overlapList newlist))
+
+prop_sky1 :: [Rectangle] -> [Rectangle] -> Bool 
+prop_sky1 a b = skylinearea (a ++ b) >= skylinearea a && skylinearea (a ++ b) >= skylinearea b 
+
+prop_skyheight :: [Rectangle] -> Property 
+prop_skyheight l = 2 * skylinearea l === skylinearea (map doubleHeight l) 
+
+prop_skyx :: [Rectangle] -> Property 
+prop_skyx l = skylinearea l === skylinearea (l ++ l) 
+
+prop_pos :: [Rectangle] -> Bool 
+prop_pos l = skylinearea l >= 0 
 
 return [] 
 runTests :: IO Bool 
