3d77db1a434eb53ea1617680cc26b39d5acb528d
May Khaw <sweemaykhaw@gmail.com>
2014-12-23 15:13:39 +0000

    updated cashflow

--- Haskellymagic/Matthias/cashflow.hs
+++ Haskellymagic/Matthias/cashflow.hs
@@ -1,5 +1,6 @@
---{-# LANGUAGE TemplateHaskell #-} 
+{-# LANGUAGE TemplateHaskell #-} 
 import Test.QuickCheck 
+import Data.List 
 
 foldright :: (a -> b -> b) -> b -> [a] -> b 
 foldright _ b [] = b 
@@ -9,20 +10,25 @@ foldleft :: (b -> a -> b) -> b -> [a] -> b
 foldleft _ b [] = b 
 foldleft f b (x : xs) = foldleft f (f b x) xs  
 
+recursiveflow :: Int -> [Int] -> [Int] 
+recursiveflow x [] = [x] 
+recursiveflow x (y : ys) = x : recursiveflow (x + y) ys 
+
+initflow :: Int -> [Int] -> [Int] 
+initflow x l = map sum $ tail $ inits (x : l) 
+
+prop_recinit :: Int -> [Int] -> Bool 
+prop_recinit x l = recursiveflow x l == initflow x l 
+
 cashflow :: Int -> [Int] -> [Int] 
-cashflow x l = scanl helper [x] l 
-    where helper [b] y = (b + y) : b 
-          helper (b : bs) y = (b + y) : b : bs 
+cashflow x l = scanl helper [] (x : l) 
+    where helper [] a = [a] 
+          helper [b] a = (a + b) : b 
+          helper (b : bs) a = (a + b) : b : bs 
 
-test_cash :: Int -> [Int] -> Bool 
-test_cash x l = if null l then head (cashflow x l) == x
-                          else head (cashflow x l) == sum (x : l) 
-
-negCashflow :: Int -> [Int] -> Either (Int,Int) Int 
-negCashflow x l = let newlist = filter (\x -> snd x < 0) $ zip [0..] cashflow x l in 
-                  if null newlist then Right $ sum (x : l) 
-                                  else Left $ head newlist 
+return [] 
+runTests :: IO Bool 
+runTests = $quickCheckAll 
 
 main :: IO Bool 
-main = do 
-    quickCheck test_cash 
+main = runTests 
