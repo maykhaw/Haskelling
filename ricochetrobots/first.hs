f7dc133b3cbf4d082266368c8a19152183a3acb3
May Khaw <sweemaykhaw@gmail.com>
2015-01-13 17:01:37 +1100

    updated with left, right, up, down

--- ricochetrobots/first.hs
+++ ricochetrobots/first.hs
@@ -12,10 +12,26 @@ data Colour = Red
 data RP = RP Position Colour
              deriving (Eq, Ord, Show) 
 
-data Wall = V Int Int -- V Up Down 
-          | H Int Int -- H Left Right 
+data Orient = V | H
+              deriving (Eq, Ord, Show) 
+
+isV :: Orient -> Bool 
+isV V = True
+isV H = False 
+
+isH :: Orient -> Bool 
+isH V = False 
+isH H = True 
+ 
+data Wall = Wall Orient Int Int 
+            deriving (Eq, Ord, Show) 
+-- Orient designates whether a wall is Vertical or Horizontal. 
+-- A Vertical x y is a line that increases in y. the line is between points (x, y) and (x, y + 1) 
+-- A Horizontal x y is a line that increases in x. the line is between points (x, y) and (x + 1, y) 
+
 
 data Position = Position Int Int 
+                deriving (Eq, Ord, Show) 
 
 -- wallList is the fixed list of walls 
 
@@ -24,11 +40,11 @@ data Position = Position Int Int
 robowalls :: [RP] -> [Wall]
 robowalls [] = [] 
 robowalls l = nub $ concatMap helper l 
-    where helper (RP (Position x y) c) = undefined  
+    where helper (RP (Position x y) c) = [Wall V x y, Wall H x y, Wall V (x + 1) y, Wall H x (y + 1)]  
 
 -- up takes a current RobotPosition and increases the y value until it hits a wall 
 up :: [Wall] -> RP -> RP 
-up walls (RP (Position x y) c) = let shortlist = sort $ filter (\(orient a b) -> orient == H && a == x && b > y) walls in 
+up walls (RP (Position x y) c) = let shortlist = sort $ filter (\(Wall orient a b) -> isH orient && a == x && b > y) walls in 
                                  case shortlist of 
                                     [] -> RP (Position x 16) c
                                     (Wall orient a b):_ -> RP (Position x (b - 1)) c 
@@ -36,25 +52,30 @@ up walls (RP (Position x y) c) = let shortlist = sort $ filter (\(orient a b) ->
 
 -- down takes a current RobotPosition and decreases the y value until it hits a wall 
 down :: [Wall] -> RP -> RP 
-down walls (RP (Position x y) c) = let shortlist = sort $ filter (\Wall orient a b -> orient == H && a == x && b < y) walls in 
+down walls (RP (Position x y) c) = let shortlist = sort $ filter (\(Wall orient a b) -> isH orient && a == x && b < y) walls in 
                                    case shortlist of 
-                                       [] -> RP (Position x 16) c
-                                       (Wall a b):_ -> RP (Position x (b - 1)) c 
+                                       [] -> RP (Position x 0) c
+                                       (Wall orient a b):_ -> RP (Position x b) c  
 
-{- -- up takes a current RobotPosition and increases the y value until it hits a wall 
-up :: [Wall] -> RP -> RP 
-up walls (RP (Position x y) c) = let shortlist = sort $ filter (\Wall a b -> a == x && b > y) walls in 
-                                 case shortlist of 
-                                    [] -> RP (Position x 16) c
-                                    (Wall a b):_ -> RP (Position x (b - 1)) c 
--- up takes a current RobotPosition and increases the y value until it hits a wall 
-up :: [Wall] -> RP -> RP 
-up walls (RP (Position x y) c) = let shortlist = sort $ filter (\Wall a b -> a == x && b > y) walls in 
-                                 case shortlist of 
-                                    [] -> RP (Position x 16) c
-                                    (Wall a b):_ -> RP (Position x (b - 1)) c -} 
+
+-- left takes a current RobotPosition and decreases the x value until it hits a wall 
+left :: [Wall] -> RP -> RP 
+left walls (RP (Position x y) c) = let shortlist = sort $ filter (\(Wall orient a b) -> isV orient && a < x && b == y) walls in 
+                                   case shortlist of 
+                                       [] -> RP (Position 0 y) c
+                                       (Wall orient a b):_ -> RP (Position a y) c 
+
+
+-- right takes a current RobotPosition and increases the x value until it hits a wall 
+right :: [Wall] -> RP -> RP 
+right walls (RP (Position x y) c) = let shortlist = sort $ filter (\(Wall orient a b) -> isV orient && a == x && b < y) walls in 
+                                    case shortlist of 
+                                       [] -> RP (Position 0 y) c
+                                       (Wall orient a b):_ -> RP (Position (a - 1) y) c  
+-- oneStep produces a list of possible new RPs, based on up, down, left and right 
 oneStep :: [Wall] -> RP -> [RP]  
-oneStep walls (RP (Position x y) c) = undefined 
+oneStep walls (RP (Position x y) c) =  
 
 prop_oneStep4 :: Position -> Bool 
 prop_oneStep4 (Position x y) = 4 >= length (oneStep (Position x y)) 
+
