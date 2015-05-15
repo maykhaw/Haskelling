443809d0ae90cf40402340a602c26870aea1e175
May Khaw <sweemaykhaw@gmail.com>
2015-05-15 23:59:53 +1000

    still need to fix type errors

--- Hackerrank/algorithms/graphs/dijkstra.hs
+++ Hackerrank/algorithms/graphs/dijkstra.hs
@@ -6,18 +6,25 @@ import qualified Data.Set as S
 import Control.Monad
 import Data.Maybe 
 
-setTuple :: (a -> Set a) -> Set a -> Set (a, a) 
-setTuple next current = let tuplefier :: Set ([a], a) -> [Set (a,a)]
-                            tuplefier l = S.toList $ S.map (\(list,x) -> zip list $ repeat x) l in 
-                        S.unions $ tuplefier $ S.map (\x -> (toList $ next x, x)) current 
+setTuple :: Ord a => (a -> Set a) -> Set a -> Set (a, a) 
+setTuple next current = let tuplefier :: Ord a => [([a], a)] -> Set (a,a)
+                            tuplefier l = S.fromList $ concatMap (\(list,x) -> zip list $ repeat x) l in 
+                        tuplefier $ map (\x -> (S.toList $ next x, x)) $ S.toList current 
 
-newMaps :: (a -> Set a) -> Set a -> Map a [a] -> Map a [a] 
-newMaps next current history = let tuples = helper :: (a, a) -> Map a [a] 
-                                   pred xs ys = if length xs < length ys then xs 
+newMaps :: Ord a => (a -> Set a) -> Set a -> Map a [a] -> Map a [a] 
+newMaps next current history = let pred xs ys = if length xs < length ys then xs 
                                                                          else ys 
-                                   helper (x,y) = M.singleton x (x : fromJust $ M.lookup y history) in 
-                               M.union history $ M.fromListWith pred $ map helper $ setTuple next current 
+                                   helper :: (a,a) -> (a, [a]) 
+                                   helper (x,y) = (x, (x : (M.findWithDefault (error "never happens") y history))) in 
+                               M.union history $ M.fromListWith pred $ S.map helper $ setTuple next current 
 
-dijkstra :: (a -> Set a) -> a -> a -> Maybe [a] 
+{- dijkstra :: Ord a => (a -> Set a) -> a -> a -> Maybe [a] 
 dijkstra next start end = 
-    let helper :: 
+    let helper :: Ord a => Set a -> Map a [a] -> Maybe [a] 
+        helper current history = let visited = M.keys history 
+                                     newPos = S.filter (\x -> S.notMember x visited) $ S.unions $ S.toList $ S.map next current  
+                                     newHist = newMaps next current history in 
+                                 if S.member end newPos then M.lookup end newHist 
+                                                        else if S.null newPos then Nothing 
+                                                                              else helper newPos $ newHist in 
+    helper (S.singleton start) $ M.empty -}
