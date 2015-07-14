72ebf5e74a30ae8fbeebbd15ce09f6f525d59849
May Khaw <sweemaykhaw@gmail.com>
2015-07-14 14:05:31 +1000

    writing helpers

--- Matthias/numberstring/patternmatch.hs
+++ Matthias/numberstring/patternmatch.hs
@@ -1,15 +1,5 @@
 import UnaryAddMul  
 
-
-opNothing :: NumExpr -> Op -> Maybe NumExpr -> Maybe NumExpr 
-opNothing x y Nothing = Nothing 
-opNothing x y (Just z) = Just $ Expr x y z 
-
-mcHelper :: NumExpr -> Op -> Maybe (NumExpr, [Either Sym Unary]) 
-    -> Maybe (NumExpr, [Either Sym Unary])
-mcHelper _ _ Nothing = Nothing   
-mcHelper num op (Just (expr, list)) = Just (Expr num op expr, list) 
-
 -- mcClose gets the string after a Left Parent Open 
 mcClose :: [Either Sym Unary] 
     -> Either (NumExpr, [Either Sym Unary]) [Either Sym Unary] 
@@ -19,4 +9,13 @@ mcClose (Right numa : Left (Ad ad) : Right numb : Left (Parent Close) : rest) =
     Right (NumExpr numa ad numb, rest) 
 mcClose (Left (Parent Open) : rest) = case mcClose rest of 
     Left remainder -> Left rest 
-    Right (numexpr, rem) ->  
+    Right (numexpr, rem) -> undefined 
+
+mcHelper :: NumExpr -> [Either Sym Unary]
+    -> Either (NumExpr, [Either Sym Unary]) [Either Sym Unary] 
+mcHelper expr [] = Left (expr, []) 
+mcHelper expr ((Left (Op (Ad ad))) : Right num : rest) = 
+    Right (NumExpr expr ad num, rest) 
+
+mcExpr :: [Either Sym Unary] -> Either (NumExpr, [Either Sym Unary]) [Either Sym Unary]  
+mcExpr (Right numa : Left (Ad ad)) = undefined 
