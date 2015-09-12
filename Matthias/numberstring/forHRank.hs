321283a83c48b8d59e16e983b69c19e2a473479b
May Khaw <sweemaykhaw@gmail.com>
2015-09-12 11:51:17 +1000

    need to add powm stuff back to current

--- Matthias/numberstring/forHRank.hs
+++ Matthias/numberstring/forHRank.hs
@@ -19,7 +19,6 @@ decToInt = Num . foldl (\a b -> 10 * a + b) 0 . map digitToInt
 
 parseNum :: Parsec String () NumExpr 
 parseNum = do
-    spaces
     unary <- option Add (parseMin <|> parseAdd) 
     x <- many1 digit 
     let Num y = decToInt x 
@@ -30,47 +29,37 @@ parseNum = do
 
 parseMin :: Parsec String () Op 
 parseMin = do
-    spaces
     satisfy (== '-')
     return Min 
 
 parseAdd :: Parsec String () Op 
 parseAdd = do
-    spaces
     satisfy (== '+') 
     return Add
 
 parseMul :: Parsec String () Op 
 parseMul = do
-    spaces
     satisfy (== '*')
     return Mul
 
 parseDiv :: Parsec String () Op
 parseDiv = do
-    spaces
     satisfy (== '/') 
     return Div
 
 parseParents :: Parsec String () NumExpr
 parseParents = do
-    spaces
     satisfy (== '(')
-    spaces
     x <- parseExpr 
-    spaces
     satisfy (== ')')
     return x 
 
 
-mdGen :: (NumExpr, [(Op, NumExpr)]) -> NumExpr
-mdGen (x, []) = x
-mdGen (x, (y : ys)) = mdGen (helper x y, ys)
-    where helper :: NumExpr -> (Op, NumExpr) -> NumExpr
-          helper x (Mul, y) = Expr x Mul y
-          helper x (Div, y) = Expr x Div y
-          helper x (_, y) = error "not Mul / Div"
-          
+exprGen :: (NumExpr, [(Op, NumExpr)]) -> NumExpr
+exprGen (x, ys) = foldr helper x ys
+    where helper :: (Op, NumExpr) -> NumExpr -> NumExpr
+          helper (op, num) expr = Expr expr op num 
+
 parseMd :: Parsec String () (Op, NumExpr)
 parseMd = do 
     op <- parseMul <|> parseDiv
@@ -81,7 +70,7 @@ parseMdExpr :: Parsec String () NumExpr
 parseMdExpr = do
     x <- parseNum
     numList <- many1 parseMd
-    return $ mdGen (x, numList) 
+    return $ exprGen (x, numList) 
 
 parseAddMin :: Parsec String () (Op, NumExpr) 
 parseAddMin = do
@@ -102,7 +91,7 @@ parseExpr :: Parsec String () NumExpr
 parseExpr = do
     x <- parseParents <|> (try parseMdExpr) <|> parseNum
     numList <- many parseAddMin
-    return $ amGen (x, numList)
+    return $ exprGen (x, numList)
 
 modNum = 10^9 + 7 
 
@@ -123,4 +112,5 @@ stringToInt x = case parse parseExpr "" x of
 
 main = do
     x <- getLine 
-    putStrLn $ show $ stringToInt x 
+    let newx = filter (/= ' ') x
+    putStrLn $ show $ stringToInt newx 
