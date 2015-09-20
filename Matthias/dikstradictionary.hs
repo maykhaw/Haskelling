3df062d796ab8a7566936f7e408c3ab6e757538d
May Khaw <sweemaykhaw@gmail.com>
2015-09-21 01:06:14 +1000

    written some tests. need to prevent QuickCheck generating empty Strings for Dictionary

--- Matthias/dikstradictionary.hs
+++ Matthias/dikstradictionary.hs
@@ -1,16 +1,16 @@
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE ScopedTypeVariables #-}
-
+import Control.Monad
 import Test.QuickCheck
 import Data.Maybe
 import qualified Data.Map.Strict as Map 
 import qualified Data.Set as S 
 import qualified Data.List as List 
 
-alpha :: Char -> S.Set Char 
-alpha x = S.fromList $ List.delete x ['a'..'z']
+alpha :: Letter -> S.Set Letter 
+alpha x = S.fromList $ List.delete x $ map (Letter $) ['a'..'z'] 
 
-splitx :: String -> S.Set ([Char], Char, [Char])
+splitx :: [Letter] -> S.Set ([Letter], Letter, [Letter])
 splitx x = let beginning = tail $ init $ List.inits x
                single = tail x 
                end = tail $ tail $ List.tails x in
@@ -18,34 +18,38 @@ splitx x = let beginning = tail $ init $ List.inits x
 
 middle f (a, b, c) = (a, f b, c) 
 
-substitutex :: ([Char], S.Set Char, [Char]) -> S.Set String  
+substitutex :: ([Letter], S.Set Letter, [Letter]) -> S.Set [Letter]  
 substitutex (a, b, c) = S.map (\x -> a ++ [x] ++ c) b  
 
 -- genPoss generates a list of possible 'words' that are one letter different from the original
-genPoss :: String -> S.Set String 
+genPoss :: [Letter] -> S.Set [Letter] 
 genPoss x = 
     foldl S.union S.empty $ S.map substitutex $ S.map (middle alpha) $ splitx x
 
-prop_PossLength :: (NonEmptyList Char) -> Bool 
+prop_PossLength :: (NonEmptyList Letter) -> Bool 
 prop_PossLength (NonEmpty l) = let ll = length l
                                    newSet = genPoss l in
                                null $ S.filter (\x -> ll /= length x) newSet 
 
-prop_PossElem :: (NonEmptyList Char) -> Bool 
+prop_PossElem :: (NonEmptyList Letter) -> Bool 
 prop_PossElem (NonEmpty l) = let ll = List.sort l
                                  newSet = S.map List.sort $ genPoss l in
                              null $ S.filter (== ll) newSet
 
 -- genKey removes 'words' that are not in the dictionary
-genKey :: S.Set String -> String -> S.Set String 
+genKey :: S.Set [Letter] -> [Letter] -> S.Set [Letter] 
 genKey dict x = S.intersection dict $ genPoss x 
 
 
-genMap :: S.Set String -> Map.Map String (S.Set String)
+genMap :: S.Set [Letter] -> Map.Map [Letter] (S.Set [Letter])
 genMap dict = Map.fromSet (genKey dict) dict
 
+prop_map :: Dictionary -> Bool
+prop_map (Dictionary dict) = 
+    all (\x -> S.member x dict) $ foldr S.union S.empty $ Map.elems $ genMap dict 
 
-wordPath :: S.Set String -> String -> String -> Maybe [String]
+
+wordPath :: S.Set [Letter] -> [Letter] -> [Letter] -> Maybe [[Letter]]
 wordPath dict start end = 
     let lstart = length start
         lend = length end
@@ -54,7 +58,9 @@ wordPath dict start end =
     in if lstart == lend then dijkstra mapping start end 
                          else Nothing
 
-newFrontier :: forall a. Ord a => Map.Map a (S.Set a) -> Map.Map a [a] -> Map.Map a [a] 
+
+newFrontier :: forall a. Ord a => 
+    Map.Map a (S.Set a) -> Map.Map a [a] -> Map.Map a [a] 
 newFrontier mapping frontier =
     let front = Map.keysSet frontier
         helper :: Ord a => (a, a) -> Map.Map a [a] -> Map.Map a [a]
@@ -88,6 +94,60 @@ dijkstra mapping start end =
                                           else helper mapset newfront fin in 
     helper mapping (Map.singleton start []) end 
 
+
+newtype Letter = Letter Char
+    deriving (Eq, Ord, Show) 
+
+instance Arbitrary Letter where
+    arbitrary = Letter <$> choose ('a', 'z')
+    shrink (Letter x) = Letter <$> filter (< x) ['a'..'z']
+
+newtype Dictionary = Dictionary (S.Set [Letter])
+    deriving (Eq, Ord, Show) 
+
+instance Arbitrary Dictionary where
+    arbitrary = do
+        (s :: [Letter]) <- arbitrary 
+        let lengths = length s 
+        (x :: [Letter]) <- replicateM lengths arbitrary 
+        (l :: [()]) <- arbitrary 
+        rest <- mapM (\_ -> replicateM lengths arbitrary) l 
+        return $ Dictionary $ S.fromList (s:x:rest)
+
+corrLength :: Int -> [Letter] -> [Letter]
+corrLength x list = let ll = length list in
+    case compare x ll of 
+        GT -> corrLength x $ list ++ take (x - ll) list 
+        EQ -> list
+        LT -> take x list 
+
+prop_corrLength :: Positive Int -> NonEmptyList Letter -> Property
+prop_corrLength (Positive x) (NonEmpty l) = x === (length $ corrLength x l)
+
+prop_validpath :: 
+    Dictionary -> NonEmptyList Letter -> NonEmptyList Letter -> Bool
+prop_validpath (Dictionary dict) (NonEmpty start) (NonEmpty end) =  
+    let ll = length $ S.elemAt 0 dict 
+        newStart = corrLength ll start
+        newEnd = corrLength ll end 
+        mapping = genMap $ foldr S.insert dict [newStart, newEnd] in 
+    case dijkstra mapping newStart newEnd of  
+        Nothing -> True 
+        Just list -> all (\x -> x `elem` dict) list 
+
+prop_shorter :: Dictionary -> Dictionary 
+    -> NonEmptyList Letter -> NonEmptyList Letter -> Bool
+prop_shorter (Dictionary dict1) (Dictionary dict2) (NonEmpty a) (NonEmpty b) =
+    let ll = length $ S.elemAt 0 dict1
+        (newA, newB) = (corrLength ll a, corrLength ll b) 
+        newDict2 = S.map (corrLength ll) dict2 
+        mapping1 = genMap $ foldr S.insert dict1 [newA, newB]
+        mapping2 = Map.union mapping1 $ genMap newDict2 in
+    case (dijkstra mapping1 newA newB, dijkstra mapping2 newA newB) of
+        (Just list1, Just list2) -> (length list1) >= (length list2)
+        (Just list1, Nothing) -> False
+        (Nothing, Just list2) -> True
+        (Nothing, Nothing) -> True
 
 
 return []
