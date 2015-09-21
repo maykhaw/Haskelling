6223be4a9075cf20c77cd589a85e19e39030dbd2
May Khaw <sweemaykhaw@gmail.com>
2015-09-21 19:11:36 +1000

    some tests are passing

--- Matthias/dikstradictionary.hs
+++ Matthias/dikstradictionary.hs
@@ -1,6 +1,8 @@
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 import Control.Monad
+import System.Environment
+import System.IO
 import Test.QuickCheck
 import Data.Maybe
 import qualified Data.Map.Strict as Map 
@@ -10,16 +12,50 @@ import qualified Data.List as List
 alpha :: Letter -> S.Set Letter 
 alpha x = S.fromList $ List.delete x $ map (Letter $) ['a'..'z'] 
 
+prop_alpha :: Letter -> Property
+prop_alpha x = (S.size $ alpha x) === 25
+
 splitx :: [Letter] -> S.Set ([Letter], Letter, [Letter])
-splitx x = let beginning = tail $ init $ List.inits x
-               single = tail x 
-               end = tail $ tail $ List.tails x in
-           S.fromList $ zip3 beginning single end
+splitx [] = S.empty
+splitx [x] = S.singleton ([], x, [])
+splitx l =  
+    let beginning = init $ List.inits l
+        middle = l
+        end = tail $ List.tails l in
+    S.fromList $ zip3 beginning middle end
+
+prop_splitword :: [Letter] -> Bool
+prop_splitword l = 
+    let helper :: ([Letter], Letter, [Letter]) -> [Letter] 
+        helper (a, b, c) = a ++ [b] ++ c in
+    and $ S.map (\x -> helper x == l) $ splitx l
+
+prop_splitsize :: [Letter] -> Property
+prop_splitsize l = 
+    length l === S.size (splitx l)
+
 
 middle f (a, b, c) = (a, f b, c) 
 
 substitutex :: ([Letter], S.Set Letter, [Letter]) -> S.Set [Letter]  
 substitutex (a, b, c) = S.map (\x -> a ++ [x] ++ c) b  
+
+prop_genSize :: [Letter] -> Bool
+prop_genSize l = 
+    let x = S.size $ genPoss l
+        ll = length l in 
+    x >= ll && x <= (ll * 26)
+
+prop_genAlpha :: NonEmptyList Letter -> Bool
+prop_genAlpha (NonEmpty l) = 
+    all (oneStep l) $ genPoss l
+
+oneStep :: [Letter] -> [Letter] -> Bool
+oneStep a b = 
+    if length a == length b then helper a b
+                            else False 
+    where helper :: [Letter] -> [Letter] -> Bool
+          helper x y = (length $ filter (\(l,r) -> l /= r) $ zip x y) == 1 
 
 -- genPoss generates a list of possible 'words' that are one letter different from the original
 genPoss :: [Letter] -> S.Set [Letter] 
@@ -49,14 +85,14 @@ prop_map (Dictionary dict) =
     all (\x -> S.member x dict) $ foldr S.union S.empty $ Map.elems $ genMap dict 
 
 
-wordPath :: S.Set [Letter] -> [Letter] -> [Letter] -> Maybe [[Letter]]
-wordPath dict start end = 
+wordPath :: Dictionary -> [Letter] -> [Letter] -> Maybe [[Letter]]
+wordPath (Dictionary dict) start end = 
     let lstart = length start
         lend = length end
         newDict = S.filter (\x -> length x == lstart) dict
-        mapping = genMap newDict
-    in if lstart == lend then dijkstra mapping start end 
-                         else Nothing
+        mapping = genMap newDict in 
+    if lstart == lend then dijkstra mapping start end 
+                      else Nothing
 
 
 newFrontier :: forall a. Ord a => 
@@ -92,7 +128,7 @@ dijkstra mapping start end =
                 Nothing -> 
                     if mapmap == newfront then Nothing 
                                           else helper mapset newfront fin in 
-    helper mapping (Map.singleton start []) end 
+    fmap reverse $ helper mapping (Map.singleton start []) end 
 
 
 newtype Letter = Letter Char
@@ -107,7 +143,7 @@ newtype Dictionary = Dictionary (S.Set [Letter])
 
 instance Arbitrary Dictionary where
     arbitrary = do
-        (s :: [Letter]) <- arbitrary 
+        (NonEmpty s :: NonEmptyList Letter) <- arbitrary 
         let lengths = length s 
         (x :: [Letter]) <- replicateM lengths arbitrary 
         (l :: [()]) <- arbitrary 
@@ -115,25 +151,18 @@ instance Arbitrary Dictionary where
         return $ Dictionary $ S.fromList (s:x:rest)
 
 corrLength :: Int -> [Letter] -> [Letter]
-corrLength x list = let ll = length list in
-    case compare x ll of 
-        GT -> corrLength x $ list ++ take (x - ll) list 
-        EQ -> list
-        LT -> take x list 
+corrLength x list = take x $ cycle list
 
 prop_corrLength :: Positive Int -> NonEmptyList Letter -> Property
 prop_corrLength (Positive x) (NonEmpty l) = x === (length $ corrLength x l)
 
-prop_validpath :: 
-    Dictionary -> NonEmptyList Letter -> NonEmptyList Letter -> Bool
-prop_validpath (Dictionary dict) (NonEmpty start) (NonEmpty end) =  
-    let ll = length $ S.elemAt 0 dict 
-        newStart = corrLength ll start
-        newEnd = corrLength ll end 
-        mapping = genMap $ foldr S.insert dict [newStart, newEnd] in 
-    case dijkstra mapping newStart newEnd of  
+prop_validpath :: NonEmptyList Letter -> NonEmptyList Letter -> Bool
+prop_validpath (NonEmpty start) (NonEmpty end) =  
+    let newEnd = corrLength (length start) end 
+        (Dictionary newDict) = genDict start newEnd in  
+    case wordPath (Dictionary newDict) start newEnd of  
         Nothing -> True 
-        Just list -> all (\x -> x `elem` dict) list 
+        Just list -> all (\x -> x `elem` newDict) list 
 
 prop_shorter :: Dictionary -> Dictionary 
     -> NonEmptyList Letter -> NonEmptyList Letter -> Bool
@@ -149,6 +178,39 @@ prop_shorter (Dictionary dict1) (Dictionary dict2) (NonEmpty a) (NonEmpty b) =
         (Nothing, Just list2) -> True
         (Nothing, Nothing) -> True
 
+letterToChar :: Letter -> Char
+letterToChar (Letter x) = x 
+
+prettify :: [Letter] -> String
+prettify = map letterToChar 
+
+charToLetter :: Char -> Letter
+charToLetter x = Letter x 
+toDict :: [String] -> Dictionary
+toDict dict =
+    Dictionary $ S.fromList $ map (map charToLetter) dict 
+
+fromDict :: Dictionary -> [String]
+fromDict (Dictionary dict) = map prettify $ S.toList dict 
+
+genDict :: [Letter] -> [Letter] -> Dictionary
+genDict [] _ = error "empty left list"
+genDict _ [] = error "empty right list"
+genDict a b = 
+    let la = length a 
+        newb = corrLength la b 
+        helper :: (Int, Letter) -> [[Letter]] -> [[Letter]]
+        helper (num, lettr) (x : rest) = 
+            (updateDict (num, lettr) x) : x : rest
+    in Dictionary $ S.fromList $ foldr helper [newb] $ zip [1..] a
+
+
+updateDict :: (Int, Letter) -> [Letter] -> [Letter]
+updateDict (num, lettr) str =
+    take (num - 1) str ++ [lettr] ++ drop num str 
+
+
+
 
 return []
 
@@ -157,3 +219,21 @@ testAll = $quickCheckAll
 
 main :: IO Bool
 main = testAll
+
+car = map charToLetter "car" 
+cat = map charToLetter "cat" 
+dog = map charToLetter "dog" 
+
+{- main = do
+    args <- getArgs
+    case args of
+        [] -> return ()
+        (filename:_) -> do
+            withFile filename ReadMode $ \handle -> do
+                content <- hGetContents handle
+                let newDict = toDict $ words content 
+                putStrLn $ case wordPath newDict cat dog of
+                    Just list -> unwords $ map prettify list
+                    Nothing -> "Nothing"
+                return () 
+            return () -}
